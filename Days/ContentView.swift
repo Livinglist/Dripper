34a85d59d2179912ed8d1002749a77e3aa065cca
livinglist <georgefung78@live.com>
2021-01-10 00:38:17 -0800

    Fix the first post not getting saved error.

--- Days/ContentView.swift
+++ Days/ContentView.swift
@@ -68,6 +68,14 @@ struct ContentView: View {
                 let newItem = Item(context: viewContext)
                 newItem.timestamp = Date()
                 newItem.text = "My first time using this app!"
+                do {
+                    try viewContext.save()
+                } catch {
+                    // Replace this implementation with code to handle the error appropriately.
+                    // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                    let nsError = error as NSError
+                    fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+                }
             }
         }
     }
